9c30db5f2a59a81d5a53e8ba7b300ae069ff4833
toruko44 <toruko5022@gmail.com>
2020-09-09 00:36:50 +0900

    todoリスト

---
+++ todo.js
@@ -0,0 +1,28 @@
+new Vue({
+  el:'#todo',
+  data:{
+    addtext:'',
+    todos:[
+    ]
+  },
+  computed:{
+    remaining:function(){
+      return this.todos.filter(function(val){
+        return val.done;
+      }).length;
+    }
+  },
+  methods:{
+    addToDo:function(){
+      if(this.addtext){
+        this.todos.push({done:false,text:this.addtext});
+        this.addtext = '';
+      }
+    },
+    cleanToDo:function(){
+      this.todos = this.todos.filter(function(val){
+        return val.done == false;
+      })
+    }
+  }
+})
